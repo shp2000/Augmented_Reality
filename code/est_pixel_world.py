d73dcc72342cd628f71d6078b528d127683dcbca
Shrehari Prsad <67973553+shp2000@users.noreply.github.com>
2023-08-31 23:07:50 -0400

    Update est_pixel_world.py

--- code/est_pixel_world.py
+++ code/est_pixel_world.py
@@ -14,6 +14,25 @@ def est_pixel_world(pixels, R_wc, t_wc, K):
     """
 
     ##### STUDENT CODE START #####
-
+    # Inverse of camera intrinsic matrix
+    K_inv = np.linalg.inv(K)
+    
+    # Convert pixels to homogeneous coordinates
+    pixels_homogeneous = np.hstack((pixels, np.ones((pixels.shape[0], 1))))
+    
+    # Transform pixel coordinates to normalized image coordinates
+    x_normalized = np.dot(K_inv, pixels_homogeneous.T).T
+    
+    # Create the direction vectors in camera coordinates (z = 0 in the world)
+    direction_c = np.hstack((x_normalized[:, :2], np.zeros((x_normalized.shape[0], 1))))
+    
+    # Transform direction vectors to world coordinates
+    direction_w = np.dot(R_wc, direction_c.T).T
+    
+    # Calculate the scale factor to account for the z-coordinate being 0
+    scale = t_wc[2] / direction_w[:, 2]
+    
+    # Calculate world coordinates
+    Pw = t_wc + scale[:, np.newaxis] * direction_w
     ##### STUDENT CODE END #####
     return Pw
